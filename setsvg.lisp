c7e79ff5ab3844cbb283e9fb79a186b11131cab3
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2017-12-29 13:28:51 -0500

    make it print an actual random circle

--- setsvg.lisp
+++ setsvg.lisp
@@ -3,6 +3,10 @@
 ; Milestone 0: print an svg
 ; Milestone 1: print a circle with a random color
 
+; Initialize the global random state.
+; https://stackoverflow.com/questions/4034042/random-in-common-lisp-not-so-random
+(setf *random-state* (make-random-state t))
+
 (defparameter *begin-svg* "<svg xmlns=\"http://www.w3.org/2000/svg\"
   xmlns:xlink=\"http://www.w3.org/1999/xlink\"
   width=\"500\" height=\"500\">")
@@ -13,8 +17,8 @@
   (concatenate 'string "<ellipse
       cx=\"250.0\"
       cy=\"250.0\"
-      rx=\"60.0\"
-      ry=\"60.0\"
+      rx=\"100.0\"
+      ry=\"100.0\"
       stroke=\"#fc8d62\"
       stroke-width=\"5\"
       fill=\"#" color "\"
@@ -23,14 +27,26 @@
 
 (defparameter *colors* '("66c2a5" "fc8d62" "8da0cb"))
 
-(defun randcolor ()
-  (let 
-    ((n (length *colors*)))
-    (car (nthcdr (random n) *colors*))
+(defun randselect (somelist)
+  (let
+    ((n (length somelist)))
+    (car (nthcdr (random n) somelist))
   )
 )
 
-(print (ellipse (randcolor)))
+(defun randcolor ()
+  (randselect *colors*)
+)
+
+(defun randcircle ()
+  (format t "~d" *begin-svg*)
+  (format t "~d" (ellipse (randcolor)))
+  (format t "~d" *end-svg*)  
+  )
+
+(randcircle)
+
+; (print (ellipse (randcolor)))
 
 ; (format t "~d" *begin-svg*)
 ; (format t "~d" *oval*)
