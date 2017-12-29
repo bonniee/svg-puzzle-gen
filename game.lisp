0c5403db6125d6e0cea3d8d5eda18b1c9e749e58
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2017-12-28 23:08:59 -0500

    more game mods

--- game.lisp
+++ game.lisp
@@ -76,7 +76,45 @@
 (defun inventory ()
   (cons 'items- (objects-at 'body *objects* *object-locations*)))
 
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+(defun game-eval (sexp)
+    (if (member (car sexp) *allowed-commands*)
+        (eval sexp)
+        '(i do not know that command.)))
+
+(defun game-read ()
+    (let ((cmd (read-from-string
+                     (concatenate 'string "(" (read-line) ")"))))
+         (flet ((quote-it (x)
+                         (list 'quote x)))
+             (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+(defun tweak-text (lst caps lit)
+   (when lst
+  (let ((item (car lst))
+         (rest (cdr lst)))
+   (cond ((eq item #\space) (cons item (tweak-text rest caps lit)))
+         ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+         ((eq item #\") (tweak-text rest caps (not lit)))
+           (lit (cons item (tweak-text rest nil lit)))
+         ((or caps lit) (cons (char-upcase item) (tweak-text rest nil lit)))
+         (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+(defun game-print (lst)
+ (princ (coerce (tweak-text (coerce (string-trim "() "
+                                                  (prin1-to-string lst))
+                                     'list)
+                             t
+                            nil)
+                 'string))
+  (fresh-line))
+
 (defun game-repl ()
-     (loop (print (eval (read)))))
+    (let ((cmd (game-read)))
+        (unless (eq (car cmd) 'quit)
+            (game-print (game-eval cmd))
+            (game-repl))))
 
 (print (look))
