ef8e2123178e4663f8bddba46e560a5207f2c10c
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2018-01-09 11:51:53 -0500

    make lein run work

--- wonderland/src/wonderland/core.clj
+++ wonderland/src/wonderland/core.clj
@@ -4,3 +4,8 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main
+  []
+  (foo "from main:")
+  )
--- wonderland/test/wonderland/core_test.clj
+++ wonderland/test/wonderland/core_test.clj
@@ -3,5 +3,5 @@
             [wonderland.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Basic test template."
+    (is (= 1 1))))
