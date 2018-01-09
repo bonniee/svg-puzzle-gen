47de9a8f5cd3acfb557ccf52cb38812fce1caf4a
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2018-01-09 14:28:57 -0500

    add voronoi to main

--- wonderland/src/wonderland/core.clj
+++ wonderland/src/wonderland/core.clj
@@ -9,5 +9,9 @@
 (defn -main
   []
   (foo "from main:")
-  
+  (let [points [[0 0] [1 1] [0 1]]
+      {:keys [points edges cells]} (voronoi/diagram points)]
+  (println "points" points 1)
+  (println "edge" edges)
+  (println "cell" cells 1))
   )
