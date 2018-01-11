8369d77068d50624c080f6531c4b6154021e0adb
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2018-01-11 15:05:00 -0500

    add circular clj

--- puzzle/project.clj
+++ puzzle/project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-exec "0.3.7"]]
   :dependencies [
   [org.clojure/clojure "1.8.0"]
   [trystan/voronoi-diagram "1.0.0"]
---
+++ puzzle/src/puzzle/circular.clj
@@ -0,0 +1,31 @@
+(ns puzzle.circular
+  (:require [puzzle.strings :as strings])
+  (:require [puzzle.svg :as svg])
+  (:require [puzzle.squiggle :as squiggle])
+  (:require [voronoi-diagram.core :as voronoi]))
+
+(defn xlen [angle radius]
+  (Math/cos (Math/toRadians angle)))
+
+(defn ylen [angle radius]
+  (Math/sin (Math/toRadians angle)))
+
+(defn anglediff [slices] (/ 360.0 slices))
+
+(defn coord-for-angle [angle radius]
+  [(xlen angle radius) (ylen angle radius)])
+
+(defn mod-angle [angle]
+  (mod angle 360))
+
+(defn angles[N offset]
+  (for [x (range N)] (+ offset (mod-angle (* x (anglediff N))))))
+
+(defn coords [N r] (map #(coord-for-angle % r) (angles N (rand-int 360))))
+
+(defn mk-circle-coords [rings]
+  (concat (reduce concat (for [ring (range rings)]
+      (coords (+ (rand-int 3) 8) (+ 1 (* 100 ring)))))
+    [[0 0]]))
+
+(println (mk-circle-coords 2))
